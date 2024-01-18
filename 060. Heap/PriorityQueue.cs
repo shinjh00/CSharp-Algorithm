c4a48d1e989439e35760f3396bff34920694486e
shinjh00 <werytuiop00@gmail.com>
2024-01-18 10:25:11 +0900

    060. Heap 완료

---
+++ 060. Heap/PriorityQueue.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class PriorityQueue<TElement, TPriority> where TPriority : IComparable<TPriority>
+    {
+        private struct Node
+        {
+            public TElement element;  // 데이터 요소
+            public TPriority priority;  // 우선 순위
+
+            public Node(TElement element, TPriority priority)
+            {
+                this.element = element;
+                this.priority = priority;
+            }
+        }
+
+        private List<Node> nodes;
+
+        public PriorityQueue()
+        {
+            nodes = new List<Node>();
+        }
+
+        public int Count { get { return nodes.Count; } }
+
+        public void Enqueue(TElement element, TPriority priority)
+        {
+            Node newNode = new Node(element, priority);
+            PushHeap(newNode);
+        }
+
+        public TElement Peek()
+        {
+            if (nodes.Count == 0)
+                throw new InvalidOperationException();
+
+            return nodes[0].element;
+        }
+
+        public TElement Dequeue()
+        {
+            if (nodes.Count == 0)
+                throw new InvalidOperationException();
+
+            Node rootNode = nodes[0];
+            PopHeap();
+            return rootNode.element;
+        }
+
+        private void PushHeap(Node node)
+        {
+            nodes.Add(node);
+            int index = nodes.Count - 1;
+            while (index > 0)  // 상승작업 : 맨 위 노드되기 전까지 반복
+            {
+                int parentIndex = GetParentIndex(index);
+                Node parentNode = nodes[parentIndex];
+
+                if (node.priority.CompareTo(parentNode.priority) < 0)
+                {
+                    nodes[index] = parentNode;
+                    index = parentIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            nodes[index] = node;
+        }
+
+        private void PopHeap()
+        {
+            Node node = nodes[nodes.Count - 1];
+            nodes[0] = node;
+            nodes.RemoveAt(nodes.Count - 1);
+
+            int index = 0;
+            while (index < nodes.Count)
+            {
+                int leftIndex = GetLeftChildIndex(index);
+                int rightIndex = GetRightChildIndex(index);
+
+                if (rightIndex < nodes.Count)
+                {
+                    int compareIndex = nodes[leftIndex].priority.CompareTo(nodes[rightIndex].priority) < 0 ?
+                        leftIndex : rightIndex;
+
+                    if (nodes[index].priority.CompareTo(nodes[compareIndex].priority) > 0)
+                    {
+                        nodes[index] = nodes[compareIndex];
+                        index = compareIndex;
+                    }
+                    else
+                    {
+                        nodes[index] = node;
+                        break;
+                    }
+                }
+                else if (leftIndex < nodes.Count)
+                {
+                    if (nodes[index].priority.CompareTo(nodes[leftIndex].priority) > 0)
+                    {
+                        nodes[index] = nodes[leftIndex];
+                        index = leftIndex;
+                    }
+                    else
+                    {
+                        nodes[index] = node;
+                        break;
+                    }
+                }
+                else
+                {
+                    nodes[index] = node;
+                    break;
+                }
+            }
+        }
+
+        private int GetParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        private int GetLeftChildIndex(int index)
+        {
+            return index * 2 + 1;
+        }
+
+        private int GetRightChildIndex(int index)
+        {
+            return index * 2 + 2;
+        }
+    }
+}
---
+++ 060. Heap/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _060._Heap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 오름차순 : 기본 int 우선순위
+            PriorityQueue<string, int> pq1 = new PriorityQueue<string, int>();
+
+            pq1.Enqueue("Data1", 1);
+            pq1.Enqueue("Data7", 7);
+            pq1.Enqueue("Data5", 5);
+            pq1.Enqueue("Data3", 3);
+            pq1.Enqueue("Data9", 9);
+
+            // pq1 : { 1, 3, 5, 7, 9 }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(pq1.Dequeue());   // output : Data1, Data3, Data5
+            }
+
+            pq1.Enqueue("Data4", 4);
+            pq1.Enqueue("Data2", 2);
+            pq1.Enqueue("Data6", 6);
+            pq1.Enqueue("Data8", 8);
+
+            while (pq1.Count > 0)
+            {
+                Console.WriteLine(pq1.Dequeue());   // output : Data2, Data4, Data6, Data7, Data8, Data9
+            }
+
+
+            // 내림차순 : int 우선순위에 * -1을 적용하여 사용
+            PriorityQueue<string, int> pq2 = new PriorityQueue<string, int>();
+
+            pq2.Enqueue("Data1", -1);
+            pq2.Enqueue("Data7", -7);
+            pq2.Enqueue("Data5", -5);
+            pq2.Enqueue("Data3", -3);
+            pq2.Enqueue("Data9", -9);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(pq2.Dequeue());   // output : Data9, Data7, Data5
+            }
+
+            pq2.Enqueue("Data4", -4);
+            pq2.Enqueue("Data2", -2);
+            pq2.Enqueue("Data6", -6);
+            pq2.Enqueue("Data8", -8);
+
+            while (pq2.Count > 0)
+            {
+                Console.WriteLine(pq2.Dequeue());   // output : Data8, Data6, Data4, Data3, Data2, Data1
+            }
+        }
+    }
+}
